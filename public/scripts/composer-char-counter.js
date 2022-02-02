5adbfd39e52df339a73163096822b427c4815cd1
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2022-02-02 02:01:06 +0000

    creating tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-  $("#tweet-text").on("keyup keydown", function() {
-    let numberOfCharacters = $(this).val().length;
-    let remainder = $(this).siblings(".counter")[0];
-    remainder.value = 140 - numberOfCharacters;
+  // --- our code goes here ---
+  $("#tweet-text").on('keyup', function() {
+    // console.log("ITS WORKING");
+    const tweetCount = $(this).val().length;
+    $("output").text(140-tweetCount);
+  })
 
-    if (remainder.value < 0) {
-      $(this).nextAll(".counter").css("color", "red");
-        } else {
-      $(this).nextAll(".counter").css("color", "grey");
-    }
-  });
-});
+})
